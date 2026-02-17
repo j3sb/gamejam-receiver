a7f7554bf90ef9adeb198f85b8a6a26680e6d9c0
Jonas <jonas.bonnaudet@gmail.com>
2026-02-17 21:59:18 +0100

    print to serial

--- main.c
+++ main.c
@@ -30,10 +30,8 @@
 
 #include "pico/stdlib.h"
 
-#include "bsp/board_api.h"
-#include "tusb.h"
-
-#include "usb_descriptors.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
@@ -42,90 +40,23 @@
 /*------------- MAIN -------------*/
 int main(void)
 {
-  board_init();
+  stdio_init_all();
 
-  // init device stack on configured roothub port
-  tud_init(BOARD_TUD_RHPORT);
+  adc_init();
 
-  if (board_init_after_tusb)
-  {
-    board_init_after_tusb();
-  }
+  // Make sure GPIO is high-impedance, no pullups etc
+  adc_gpio_init(26);
+  // Select ADC input 0 (GPIO26)
+  adc_select_input(0);
 
   // turn on led
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
   gpio_put(PICO_DEFAULT_LED_PIN, true);
 
-  tud_task(); // tinyusb device task
-  // skip if hid is not ready yet
-  while (!tud_hid_ready())
-  {
-    tud_task(); // tinyusb device task
-  }
-
   while (1)
   {
-    tud_task(); // tinyusb device task
-
-    // moving the mouse
-    double t = board_millis() / 1000.0;
-
-    tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, sin(t) * 2, cos(t) * 2, 0, 0);
-    sleep_ms(10);
+    int a = adc_read();
+    printf("%i\n", a);
   }
-}
-
-//--------------------------------------------------------------------+
-// Device callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device is mounted
-void tud_mount_cb(void) {}
-
-// Invoked when device is unmounted
-void tud_umount_cb(void) {}
-
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en) { (void)remote_wakeup_en; }
-
-// Invoked when usb bus is resumed
-void tud_resume_cb(void) {}
-
-// Invoked when sent REPORT successfully to host
-// Application can use this to send the next report
-// Note: For composite reports, report[0] is report ID
-void tud_hid_report_complete_cb(uint8_t instance, uint8_t const *report,
-                                uint16_t len)
-{
-  (void)instance;
-  (void)len;
-}
-
-// Invoked when received GET_REPORT control request
-// Application must fill buffer report's content and return its length.
-// Return zero will cause the stack to STALL request
-uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id,
-                               hid_report_type_t report_type, uint8_t *buffer,
-                               uint16_t reqlen)
-{
-  // TODO not Implemented
-  (void)instance;
-  (void)report_id;
-  (void)report_type;
-  (void)buffer;
-  (void)reqlen;
-
-  return 0;
-}
-
-// Invoked when received SET_REPORT control request or
-// received data on OUT endpoint ( Report ID = 0, Type = 0 )
-void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id,
-                           hid_report_type_t report_type, uint8_t const *buffer,
-                           uint16_t bufsize)
-{
-  (void)instance;
 }
