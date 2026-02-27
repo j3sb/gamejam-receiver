ee00d998e81d16d0bfc5bea97bb7eaa0faddbdab
Jonas <jonas.bonnaudet@gmail.com>
2026-02-27 23:18:30 +0100

    frequency measurement ish

--- reader.py
+++ reader.py
@@ -12,13 +12,22 @@ X = np.arange(hist_size)
 
 graph = plt.plot(X, data)[0]
 
+mid = 256 / 2
+
+was_high = False
 while True:
     start = time.time()
+    switches = 0
     for i in range(hist_size):
         # Read single byte
         byte = ser.read(1)
 
         data[i] = byte[0]
+        if byte[0] > mid and not was_high:
+            switches += 1
+            was_high = True
+        else:
+            was_high = False
 
         # if byte:
         #     print(f"Got byte: {byte[0]:02X}")
@@ -29,4 +38,6 @@ while True:
     plt.draw()
     plt.pause(1e-3)
     end = time.time()
-    print(f"sampling {1 / (end - start) / 1000 * hist_size} khz")
+    sampling_speed = 1 / (end - start) / 1000 * hist_size
+    print(
+        f"freq: {switches / hist_size} sampling: {sampling_speed:.2f} khz")
